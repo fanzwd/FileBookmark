a7cf15da27f06e759d5d91a3d06eb73c72417fd8
wdz <fanzwd@live.com>
2025-06-29 20:11:09 +0800

    Initial Commit

---
+++ FileBookmark/FileBookmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileBookmarkApp.swift
+//  FileBookmark
+//
+//  Created by wdz on 2025/6/29.
+//
+
+import SwiftUI
+
+@main
+struct FileBookmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
