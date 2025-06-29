1749305039868725003c1a3e19d9a13d88aa9512
wdz <fanzwd@live.com>
2025-06-29 20:42:55 +0800

    [feature] support read file content from pasteboard

--- FileBookmark/ContentView.swift
+++ FileBookmark/ContentView.swift
@@ -10,10 +10,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                let pb = NSPasteboard.general
+                let item = pb.pasteboardItems?[0]
+                let fileURLData = item!.data(forType: .fileURL)
+                let filePath = String(data: fileURLData!, encoding: .utf8)
+                let fileURL = URL(fileURLWithPath: filePath!)
+                let bookmarkData = try! fileURL.bookmarkData(options: .withSecurityScope,
+                                                             includingResourceValuesForKeys: nil,
+                                                             relativeTo: nil)
+                UserDefaults.standard.set(bookmarkData, forKey: "bookmark")
+            } label: {
+                Text("读取剪切板")
+            }
+            
+            Button {
+                let bookmarkData = UserDefaults.standard.data(forKey: "bookmark")
+                var isStale = false
+                let url = try? URL.init(resolvingBookmarkData: bookmarkData!,
+                                         options: .withSecurityScope,
+                                         relativeTo: nil,
+                                         bookmarkDataIsStale: &isStale);
+                if (url != nil) {
+                    _ = url!.startAccessingSecurityScopedResource()
+                    let content = try? String(contentsOf: url!, encoding: .utf8)
+                    print("----> content is: \(content ?? "Empty")")
+                    url!.stopAccessingSecurityScopedResource()
+                }
+                
+            } label: {
+                Text("读取文件内容")
+            }
+
         }
         .padding()
     }
